15daaa35cab262f80071cb308ceefbbf1bef396a
robert <robert@DESKTOP-3RQNPSR>
2017-02-19 13:45:15 -0800

    Add project files.

---
+++ ItemAcquistionAPI/Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemAcquistionAPI {
+	public class Item {
+		public String name;
+		public int count;
+		public List<Item> requiredItems = new List<Item>();
+		public String comment;
+
+		public List<Item> getAllItems() {
+			List<Item> allItems = this.getItems();
+			for (int i = 0; i < allItems.Count; i++) {
+				Item tmpItem = allItems[i];
+				allItems.RemoveAt(i);
+				for(int g = 0; g< allItems.Count; g++) {
+					if(tmpItem.name == allItems[g].name) {
+						tmpItem.count = +allItems[g].count;
+						allItems.RemoveAt(g);
+					}
+				}
+				allItems.Add(tmpItem);
+			}
+			return allItems;
+		}
+
+		private List<Item> getItems() {
+			List<Item> allItems = new List<Item>();
+			for (int i = 0; i < requiredItems.Count; i++) {
+				allItems.Add(requiredItems[i]);
+				allItems.AddRange(requiredItems[i].getItems());
+			}
+
+			return allItems;
+		}
+
+		public String toString() {
+			return name;
+		}
+	}
+}
---
+++ ItemAcquistionCMD/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemAcquistionCMD {
+	class Program {
+		static void Main(string[] args) {
+			ItemAcquistionAPI.Item ia = new ItemAcquistionAPI.Item();
+			ia.name = "a";
+			ia.count = 1;
+
+			ItemAcquistionAPI.Item ia1 = new ItemAcquistionAPI.Item();
+			ia1.name = "a1";
+			ia1.count = 1;
+
+			ItemAcquistionAPI.Item ia2 = new ItemAcquistionAPI.Item();
+			ia2.name = "a2";
+			ia2.count = 1;
+
+			ItemAcquistionAPI.Item ia3 = new ItemAcquistionAPI.Item();
+			ia3.name = "a3";
+			ia3.count = 1;
+
+			ItemAcquistionAPI.Item ia31 = new ItemAcquistionAPI.Item();
+			ia31.name = "a3-1";
+			ia31.count = 1;
+
+			ia.requiredItems.Add(ia1);
+			ia.requiredItems.Add(ia2);
+			ia.requiredItems.Add(ia3);
+			ia3.requiredItems.Add(ia31);
+
+			Console.WriteLine(ia.getAllItems().ToString());
+			Console.Read();
+		}
+	}
+}
