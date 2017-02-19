3f7733bd97c80c2069fd28adbd26cd2fcc8f9df0
Rina <No-Reply@rinaday.ca>
2017-02-19 14:15:50 -0800

    Added various sorts

--- ItemAcquistionAPI/Item.cs
+++ ItemAcquistionAPI/Item.cs
@@ -6,24 +6,27 @@ using System.Threading.Tasks;
 
 namespace ItemAcquistionAPI {
 	public class Item {
-		public String name;
-		public int count;
+		public String name = "";
+		public int numberRequired;
+		public int numberHave= 0;
 		public List<Item> requiredItems = new List<Item>();
-		public String comment;
+		public String comment = "";
 
 		public List<Item> getAllItems() {
 			List<Item> allItems = this.getItems();
+			sortByName(allItems);
 			for (int i = 0; i < allItems.Count; i++) {
 				Item tmpItem = allItems[i];
 				allItems.RemoveAt(i);
 				for(int g = 0; g< allItems.Count; g++) {
 					if(tmpItem.name == allItems[g].name) {
-						tmpItem.count = +allItems[g].count;
+						tmpItem.numberRequired = tmpItem.numberRequired + allItems[g].numberRequired;
 						allItems.RemoveAt(g);
 					}
 				}
 				allItems.Add(tmpItem);
 			}
+			sortByTrulyRequired(allItems);
 			return allItems;
 		}
 
@@ -39,6 +42,23 @@ namespace ItemAcquistionAPI {
 
 		public String toString() {
 			return name;
+		}
+		private void sortByName(List<Item> listToSort) {
+			listToSort.Sort(delegate (Item x, Item y) {
+				return x.name.CompareTo(y.name);
+			});
+		}
+		private void sortByNumberRequired(List<Item> listToSort) {
+			listToSort.Sort(delegate (Item x, Item y) {
+				return x.numberRequired.CompareTo(y.numberRequired);
+			});
+		}
+		private void sortByTrulyRequired(List<Item> listToSort) {
+			listToSort.Sort(delegate (Item x, Item y) {
+				int a = x.numberRequired - x.numberHave;
+				int b = y.numberRequired - y.numberHave;
+				return b.CompareTo(a);
+			});
 		}
 	}
 }
--- ItemAcquistionCMD/Program.cs
+++ ItemAcquistionCMD/Program.cs
@@ -9,28 +9,33 @@ namespace ItemAcquistionCMD {
 		static void Main(string[] args) {
 			ItemAcquistionAPI.Item ia = new ItemAcquistionAPI.Item();
 			ia.name = "a";
-			ia.count = 1;
+			ia.numberRequired = 1;
 
 			ItemAcquistionAPI.Item ia1 = new ItemAcquistionAPI.Item();
 			ia1.name = "a1";
-			ia1.count = 1;
+			ia1.numberRequired = 1;
 
 			ItemAcquistionAPI.Item ia2 = new ItemAcquistionAPI.Item();
 			ia2.name = "a2";
-			ia2.count = 1;
+			ia2.numberRequired = 1;
 
 			ItemAcquistionAPI.Item ia3 = new ItemAcquistionAPI.Item();
 			ia3.name = "a3";
-			ia3.count = 1;
+			ia3.numberRequired = 1;
 
 			ItemAcquistionAPI.Item ia31 = new ItemAcquistionAPI.Item();
 			ia31.name = "a3-1";
-			ia31.count = 1;
+			ia31.numberRequired = 1;
+
+			ItemAcquistionAPI.Item ia11 = new ItemAcquistionAPI.Item();
+			ia11.name = "a3-1";
+			ia11.numberRequired = 1;
 
 			ia.requiredItems.Add(ia1);
 			ia.requiredItems.Add(ia2);
 			ia.requiredItems.Add(ia3);
 			ia3.requiredItems.Add(ia31);
+			ia1.requiredItems.Add(ia11);
 
 			Console.WriteLine(ia.getAllItems().ToString());
 			Console.Read();
